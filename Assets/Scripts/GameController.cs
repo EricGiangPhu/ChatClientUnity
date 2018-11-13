c196e710bd771b020d0376a4aa5414b91867a310
PHUGIANG <phugiang123@gmail.com>
2018-11-14 00:42:03 +0700

    Create client chat using C# websocket and unity 3D

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject ChatView;
+	public GameObject Container;
+
+	public SocketManager socketManager; 
+
+	public GameObject ElementMessage;
+	public InputField inputTextFild;
+	void Start () {
+		deActiveChatView();
+		socketManager.callbackMessage+= addMessageToUI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void activeChatView() {
+		ChatView.SetActive(true);
+	}
+
+	public void deActiveChatView() {
+		ChatView.SetActive(false);
+	}
+
+	public void SendMessage() {		
+		socketManager.sendWSMessage(inputTextFild.text);
+		inputTextFild.text = "";
+	}
+
+	public void addMessageToUI(string message) {
+		GameObject chatMessage = Instantiate(ElementMessage, Container.transform);
+		chatMessage.GetComponentInChildren<Text>().text = message;
+	}
+}
---
+++ Assets/Scripts/SocketManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SocketManager : MonoBehaviour {
+	public delegate void CallbackMessage(string message);
+
+	public CallbackMessage callbackMessage;
+	public string ipAddress = "ws://localhost/ws";
+
+	WebSocket ws;
+
+
+	void Awake() {
+		callbackMessage = defaultReciveMessage;
+	}
+	// Use this for initialization
+	IEnumerator Start () {
+		int countMessage = 0;
+		ws = new WebSocket(new System.Uri(ipAddress));
+		yield return StartCoroutine(ws.Connect());
+
+		while (true)
+		{
+			string revMessage = ws.RecvString();
+			if(revMessage != null) {				
+				callbackMessage(revMessage);
+				countMessage++;
+			}
+
+			if(ws.error !=null) {				
+				callbackMessage("Connect Error");
+				callbackMessage(ws.error);
+				Debug.Log("Error connect!");
+				break;
+			}
+
+			yield return 0;
+		}
+
+		ws.Close();
+	}
+	
+	public void sendWSMessage(string message) {
+		if(ws != null) {
+			ws.SendString(message);
+		}
+	}
+	private void defaultReciveMessage(string message) {
+		Debug.Log("callback is default!");
+	}
+}
